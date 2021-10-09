45fc9ab1ead59b8a80ea4b1036c19690a34b319d
valeritromycin <valeritromycin@gmail.com>
2021-10-09 13:11:10 +0300

    task 1 is done

---
+++ homework_2_task1.py
@@ -0,0 +1,37 @@
+users = (
+    {
+        "name": "Jon",
+        "surname": "Smith",
+        "age": 6,
+    },
+    {
+        "name": "Bill",
+        "surname": "Suns",
+        "age": 20,
+    }
+)
+templates = (
+    "{name} {surname} закончил школу",
+    "{name} {surname} скоро пойдет в школу",
+)
+finaltext1 = templates[1].format(name = users[0]['name'], surname = users[0]['surname'])
+print(finaltext1)
+
+finaltext2 = templates[0].format(name = users[1]['name'], surname = users[1]['surname'])
+print(finaltext2)
+
+
+
+
+
+result = templates[users[0]["age"] < 7].format(
+    name=users[0]["name"],
+    surname=users[0]["surname"],
+)
+print(result)
+
+result = templates[users[1]["age"] < 7].format(
+    name=users[1]["name"],
+    surname=users[1]["surname"],
+)
+print(result)
