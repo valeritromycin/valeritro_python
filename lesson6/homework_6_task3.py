c851bb0ca7f3052b76c3f06bdfa911d6af7be0c8
valeritromycin <valeritromycin@gmail.com>
2021-10-31 19:34:39 +0300

    Commited what wasn't commited

--- lesson6/homework_6_task3.py
+++ lesson6/homework_6_task3.py
@@ -1,11 +1,11 @@
 some_text = "Пал, а норов худ и дух ворона лап."
 
 
-def pal_check(str)
-    new_str = str.replace(' ', '')
-    new_str = new_str.replace(',', '')
-    new_str = new_str.replace('.', '')
-    l = len(str)
-    l = l / 2
-    for i in new_str:
-        if
+def pal_check(str):
+    new_str = str.replace(' ', '').replace(',', '').replace('.', '')
+    new_str = new_str.lower()
+    return new_str == new_str[::-1]
+
+
+print(pal_check(some_text))
+
